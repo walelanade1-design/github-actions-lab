7fc032dc69bcbb80f9f7880d68271ded20112387
walelanade1-design <wale.lanade1@gmail.com>
2026-03-21 11:19:24 +0000

    Add initial Terraform configuration

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Simple S3 bucket to demonstrate GitHub Actions automation
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+variable "bucket_suffix" {
+  description = "Unique suffix for bucket name (use your initials)"
+  type        = string
+  default     = "ola"  # CHANGE THIS to your initials!
+}
+
+# S3 Bucket - using fixed name to prevent duplicates
+resource "aws_s3_bucket" "demo" {
+  bucket = "cloudburst-demo-${var.bucket_suffix}"
+
+  tags = {
+    Name        = "CloudBurst Demo Bucket"
+    Environment = "dev"
+    ManagedBy   = "terraform"
+    DeployedBy  = "github-actions"
+  }
+}
+
+# Output the bucket name
+output "bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.demo.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the created S3 bucket"
+  value       = aws_s3_bucket.demo.arn
+}
